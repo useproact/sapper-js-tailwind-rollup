93388f87a2c6fb8a892d9d87df342d4e1a1d022f
Jerric Lyns John <jerriclynsjohn@outlook.com>
2020-09-07 22:25:30 +0530

    chore(linting): adding ESLint, Prettier and lint-staged and husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    purge: ['./src/**/*.svelte', './src/**/*.html'],
-    theme: {
-      extend: {},
-    },
-    variants: {},
-    plugins: [],
-}
+	purge: ['./src/**/*.svelte', './src/**/*.html'],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
