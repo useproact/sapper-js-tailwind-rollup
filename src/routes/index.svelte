25c042c40c5c85dc97381f004a7c4fdc185e5cb9
Jerric Lyns John <jerriclynsjohn@outlook.com>
2020-09-08 00:16:32 +0530

    component changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,10 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1 class="text-6xl">Great success!</h1>
+<h1 class="text-5xl text-gray-700">Great success!</h1>
 
 <figure>
-	<img alt='Success Kid' src='successkid.jpg'>
+	<img alt="Success Kid" src="successkid.jpg" />
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
