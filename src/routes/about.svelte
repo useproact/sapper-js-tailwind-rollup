68e9573f5dc513d94ccaf9d115dc47fa59b1fffd
Jerric Lyns John <jerriclynsjohn@outlook.com>
2021-02-20 00:40:26 +0530

    fix(lint): lint stage final fixes

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,11 +3,5 @@
 </svelte:head>
 
 <h1>About this site</h1>
-<p>This is new here</p>
-<p>This is new here</p>
-<p>This is new here</p>
-<p>This is new here</p>
-
-<p>This is new here</p>
 
 <p>This is the 'about' page. There's not much here.</p>
